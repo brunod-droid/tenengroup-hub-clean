12748655ae3d77ed1408aadb4fc2497078295f0d
brunod-droid <bruno.d@tenengroup.com>
2026-04-23 16:15:50 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import './styles.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <div style={{ background: "red", color: "white", padding: 10 }}>
-        TEST APP UPDATED
-      </div>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
